8b7b8c677c5974c5cc1c87f9561903adcdc54ea5
Mohd Zaid <77684943+Xaid-vfx@users.noreply.github.com>
2023-06-03 01:01:22 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (5.42%) - LeetHub

---
+++ 783-minimum-distance-between-bst-nodes/783-minimum-distance-between-bst-nodes.java
@@ -0,0 +1,10 @@
+class Solution {
+    Integer res = Integer.MAX_VALUE, pre = null;
+    public int minDiffInBST(TreeNode root) {
+        if (root.left != null) minDiffInBST(root.left);
+        if (pre != null) res = Math.min(res, root.val - pre);
+        pre = root.val;
+        if (root.right != null) minDiffInBST(root.right);
+        return res;
+    }
+}
