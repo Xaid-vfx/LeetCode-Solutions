39b0543b3dd0187c5328c7d37b6985a6e735a8ba
Mohd Zaid <77684943+Xaid-vfx@users.noreply.github.com>
2023-06-16 23:57:19 +0530

    Time: 72 ms (7.65%), Space: 45.5 MB (22.89%) - LeetHub

--- 77-combinations/77-combinations.java
+++ 77-combinations/77-combinations.java
@@ -1,15 +1,15 @@
 class Solution {
     public void helper(List <List<Integer>> list, List<Integer> temp, int j, int n, int k){
-        if(temp.size() == k){
+        System.out.println(temp);
+        if(k == 0){
             list.add(new ArrayList(temp));
             return;
         }
-        for(int i = j; i <= n; i++){
+        for(int i = j; i <= n - k + 1; i++){
             temp.add(i);
-            helper(list, temp, i + 1, n, k);
+            helper(list, temp, i + 1, n, k - 1);
             temp.remove(temp.size() - 1);
         }
-
     }
     public List<List<Integer>> combine(int n, int k) {
         List <List<Integer>> list =  new ArrayList <List<Integer>>();
